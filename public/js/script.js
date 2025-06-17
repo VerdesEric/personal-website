8a8360ba31f2c8872e811a3d6c9adecef4dff17e
VerdesEric <142631739+VerdesEric@users.noreply.github.com>
2025-06-17 06:27:59 +0000

    commit changes added new logo

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,21 @@
+//scroll to top button
 const scrollBtn = document.getElementById("scrolltotop");
 
 scrollBtn.onclick = function () {
     window.scrollTo({ top:0, behavior: "smooth" })
 };
+
+// //scale section
+// const revealObserver = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("visible");
+//     }
+//   });
+// }, {
+//   threshold: 0.3
+// });
+
+// document.querySelectorAll(".reveal-on-scroll").forEach(el => {
+//   revealObserver.observe(el);
+// });
